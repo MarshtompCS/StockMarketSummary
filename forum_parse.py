cd52c9dcdbf40c238a0bc08d43be65916cb3e989
MyLittleChange <38368378+MyLittleChange@users.noreply.github.com>
2022-04-27 18:56:41 +0800

    Update forum_parse.py

--- forum_parse.py
+++ forum_parse.py
@@ -6,8 +6,8 @@ from tqdm import tqdm
 # 帖子将作为点评文本生成模型的粗粒度预训练语料
 
 BASE_URL = "https://www.55188.com/"
-POSTS_DIR = "./data/posts_data"
-RAW_POSTS_DIR = "./data/posts_raw_data"
+POSTS_DIR = "./posts_data"
+RAW_POSTS_DIR = "./posts_raw_data"
 MAX_POST_PER_DAY = 100
 
 
@@ -24,27 +24,27 @@ def main():
     # 3. check the parsed posts whether had saved previous
     total_posts = 0
     post_per_day = {}
-    # previous_files = os.listdir(POSTS_DIR)
-    # previous_ids = []
-    # for file in previous_files:
-    #     id = file.split('_')[-1].split('.')[0]
-    #     previous_ids.append(id)
-    #     year_month_day = file.split('_')[0]
-    #     if year_month_day not in POST_PER_DAY_DICT.keys():
-    #         POST_PER_DAY_DICT[year_month_day] = 1
-    #         TOTAL_NUM += 1
-    #     else:
-    #         if POST_PER_DAY_DICT[year_month_day] >= MAX_POST_PER_DAY:
-    #             continue
-    #         else:
-    #             POST_PER_DAY_DICT[year_month_day] += 1
-    #             TOTAL_NUM += 1
+    previous_files = os.listdir(POSTS_DIR)
+    previous_ids = []
+    for file in previous_files:
+        id = file.split('_')[-1].split('.')[0]
+        previous_ids.append(id)
+        year_month_day = file.split('_')[0]
+        if year_month_day not in post_per_day.keys():
+            post_per_day[year_month_day] = 1
+            total_posts += 1
+        else:
+            if post_per_day[year_month_day] >= MAX_POST_PER_DAY:
+                continue
+            else:
+                post_per_day[year_month_day] += 1
+                total_posts += 1
 
     pbar = tqdm(total=1557)
     parse_list_url = BASE_URL + 'forum-8-t2.html'
     parsed_posts, post_htmls, total_posts, post_per_day = parse_post_list(require_day=None,
                                                                           post_list_url=parse_list_url,
-                                                                          previous_ids=[],
+                                                                          previous_ids=previous_ids,
                                                                           total_posts=total_posts,
                                                                           post_per_day=post_per_day)
     pbar.set_description('TOTAL %d' % total_posts)
@@ -57,7 +57,7 @@ def main():
         parse_list_url = BASE_URL + 'forum-8-t2-' + str(i) + '.html'
         parsed_posts, post_htmls, total_posts, post_per_day = parse_post_list(require_day=None,
                                                                               post_list_url=parse_list_url,
-                                                                              previous_ids=[],
+                                                                              previous_ids=previous_ids,
                                                                               total_posts=total_posts,
                                                                               post_per_day=post_per_day)
         pbar.update(1)
